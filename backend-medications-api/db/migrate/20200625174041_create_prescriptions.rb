6032a2f4d1114e8b1fa69e29c4e29a5df1b3ea57
ruizj3 <ruizj3@uw.edu>
2020-06-25 11:19:02 -0700

    Update 20200625174041_create_prescriptions.rb
    added correct data tyoe to prescriptions

--- backend-medications-api/db/migrate/20200625174041_create_prescriptions.rb
+++ backend-medications-api/db/migrate/20200625174041_create_prescriptions.rb
@@ -1,8 +1,8 @@
 class CreatePrescriptions < ActiveRecord::Migration[6.0]
   def change
     create_table :prescriptions do |t|
-      t.number :dosagestotal
-      t.number :dosagesper
+      t.integer :dosagestotal
+      t.integer :dosagesper
       t.string :dosagesfrequency
       t.references :patient, null: false, foreign_key: true
       t.references :medication, null: false, foreign_key: true
