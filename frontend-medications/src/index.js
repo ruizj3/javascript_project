0830f2ea0f56b5c55fcf66cf63d9289eae782af4
ruizj3 <ruizj3@uw.edu>
2020-07-07 18:37:06 -0700

    Update index.js
    setting up tests for index.js to fetch properly

--- frontend-medications/src/index.js
+++ frontend-medications/src/index.js
@@ -1,1 +1,6 @@
-console.log("testing...")
+console.log("testing...2")
+
+const BACKEND_URL = 'localhost:3000';
+fetch(`${BACKEND_URL}/test`)
+  .then(response => response.json())
+  .then(parsedResponse => console.log(parsedResponse));
