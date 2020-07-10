327ce187a73bb95720fd042de492f97a480cc86e
ruizj3 <ruizj3@uw.edu>
2020-07-09 20:34:55 -0700

    Update index.js
    starter functions that fetch data and can filter on button selections/create buttons based on API response.

--- frontend-medications/src/index.js
+++ frontend-medications/src/index.js
@@ -1,6 +1,52 @@
-console.log("testing...2")
+document.addEventListener("DOMContentLoaded", appendData)
 
-const BACKEND_URL = 'localhost:3000';
-fetch(`${BACKEND_URL}/test`)
-  .then(response => response.json())
-  .then(parsedResponse => console.log(parsedResponse));
+const BACKEND_URL = 'http://localhost:3000';
+
+
+fetch(`${BACKEND_URL}/doctors`)
+.then(function (response) {
+  return response.json();
+})
+.then(function (data) {
+  appendData(data);
+})
+.catch(function (err) {
+  console.log(err);
+});
+
+
+function appendData(data) {
+  const doctors = document.getElementById("doctors");
+  const patients = document.getElementById("patients");
+  for (var i = 0; i < data.data.length; i++) {
+      const y = document.getElementById("doctors") ;
+      y.innerHTML += "<button id = " + data.data[i].id +" "+ "class="+"btn"+ " onclick="+"filterSelection(this.id)"+ ">" + data.data[i].attributes.username + "</button>"
+  }
+}
+
+function filterSelection(id) {
+  fetch(`${BACKEND_URL}/patients`)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    for (var i = 0; i < data.data.length; i++){
+      const doctor = data.data[i].attributes.doctor.id;
+      const patients = document.getElementById("patients");
+      if (document.getElementById("patients") != '')
+      if (doctor == id) {
+        const ul = document.createElement("ul");
+        ul.innerHTML += "<button id =" + data.data[i].id + " class="+"btn"+ ">" + data.data[i].attributes.username + ' Date of Birth: ' + data.data[i].attributes.dob + "</button>"
+        patients.appendChild(ul);
+      }
+    }
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
+
+}
+
+function removeAll(){
+    document.getElementById("patients").innerHTML = "";
+}
